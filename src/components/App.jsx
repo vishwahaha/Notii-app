453d8721d40a31520154919ca493e06860b2e7e5
vishwahaha <76246039+vishwahaha@users.noreply.github.com>
2021-03-23 18:51:22 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
@@ -20,6 +20,16 @@ function App() {
       });
     });
   }
+   useEffect(() => {
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    if (notes) {
+      setNotes(notes);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
 
   return (
     <div>
